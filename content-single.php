5a3bd636493b369e43a07878c25a988b319a0163
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-03-13 10:35:37 -0700

    Demos of how to display Advanced Custom Fields and Custom Taxonomies. Look in content-single.php

--- content-single.php
+++ content-single.php
@@ -6,14 +6,45 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
+        
+        <h1 class="entry-title">
+		  <?php the_title(); ?> 
+            <span class="creator">by 
+                <?php the_field('first_name'); ?> <?php the_field('last_name'); ?>
+            </span>
+        </h1>
+        
+        <div class="meta-content">
+            <span class="time"><?php 
+echo get_the_term_list( $post->ID, 'durations', '', ', ' ); 
+                ?></span> | 
+            <span class="genre">
+                <?php 
+echo get_the_term_list( $post->ID, 'genres', '', ', ' ); 
+                ?></span>
+            
+            | <span class="trailer-link"><a href="#">Trailer</a></span>
+            
+        </div><!-- .meta-content -->
+        
+        
+        
+        
 		<div class="entry-meta">
 			<?php mod_posted_on(); ?>
 		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
+        <?php
+        // check if the post has a Post Thumbnail assigned to it.
+            if ( has_post_thumbnail() ) {
+                echo '<figure class="poster">';
+                the_post_thumbnail();
+                echo '</figure>';
+            } 
+        ?>
+        
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
