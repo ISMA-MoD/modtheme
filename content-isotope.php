97e97cb7e814502183c8ac9e3fdf58b5ca8c39f8
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-04-06 21:22:19 -0700

    Output category and taxonomy terms as class names in .item container for isotope filtering.

--- content-isotope.php
+++ content-isotope.php
@@ -4,7 +4,7 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" class="item">
+<article id="post-<?php the_ID(); ?>" class="item <?php echo mod_create_tax_classes(); ?>">
     
     <a href="<?php echo esc_url( get_permalink() ); ?>">
     
