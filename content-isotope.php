69f3bb4e4fb322a9fefda9f0837ee6cff3c1f6cf
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-04-06 19:45:37 -0700

    Display poster size image on index

--- content-isotope.php
+++ content-isotope.php
@@ -12,7 +12,7 @@
             
             <?php 
             if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-                the_post_thumbnail();
+                the_post_thumbnail('mod-poster');
             } 
             ?>
             
