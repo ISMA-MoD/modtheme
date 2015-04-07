5c68730484319955f4a569e5877cad354a69c5e5
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2015-04-06 19:52:19 -0700

    Display dummy poster when no poster is available.

--- content-isotope.php
+++ content-isotope.php
@@ -13,7 +13,9 @@
             <?php 
             if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
                 the_post_thumbnail('mod-poster');
-            } 
+            } else {
+                echo '<img width="300" height="464" src="' . get_template_directory_uri() . '/images/poster-dummy.png">';
+            }
             ?>
             
             <div class="infocontainer">
